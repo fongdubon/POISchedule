476465a5ea99dbcbf005348292a56afa809538f8
Karly <karRlyi123@gmail.com>
2020-03-26 12:29:00 -0600

    Agregar entidad Subject

---
+++ POISchedule/Data/Entities/Subject.cs
@@ -0,0 +1,12 @@
+﻿namespace POISchedule.Data.Entities
+{
+    public class Subject:IEntity
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+        public string Key { get; set; }
+        public double Credits { get; set; }
+
+    }
+}
