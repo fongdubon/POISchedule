10c5c3c111af403de9150dc5103234adc01dd97d
AlejandroRamos02 <aleej.ramos24@gmail.com>
2020-03-27 14:45:20 -0600

    Creación del controlador y vistas de Coordinator y modificacion de Layout

---
+++ POISchedule/Controllers/CoordinatorsController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using POISchedule.Data;
+using POISchedule.Data.Entities;
+
+namespace POISchedule.Controllers
+{
+    public class CoordinatorsController : Controller
+    {
+        private readonly DataContext _context;
+
+        public CoordinatorsController(DataContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Coordinators
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Coordinators.ToListAsync());
+        }
+
+        // GET: Coordinators/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var coordinator = await _context.Coordinators
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (coordinator == null)
+            {
+                return NotFound();
+            }
+
+            return View(coordinator);
+        }
+
+        // GET: Coordinators/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Coordinators/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,FirstName,LastName,ImageUrl")] Coordinator coordinator)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(coordinator);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(coordinator);
+        }
+
+        // GET: Coordinators/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var coordinator = await _context.Coordinators.FindAsync(id);
+            if (coordinator == null)
+            {
+                return NotFound();
+            }
+            return View(coordinator);
+        }
+
+        // POST: Coordinators/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,FirstName,LastName,ImageUrl")] Coordinator coordinator)
+        {
+            if (id != coordinator.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(coordinator);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CoordinatorExists(coordinator.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(coordinator);
+        }
+
+        // GET: Coordinators/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var coordinator = await _context.Coordinators
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (coordinator == null)
+            {
+                return NotFound();
+            }
+
+            return View(coordinator);
+        }
+
+        // POST: Coordinators/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var coordinator = await _context.Coordinators.FindAsync(id);
+            _context.Coordinators.Remove(coordinator);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CoordinatorExists(int id)
+        {
+            return _context.Coordinators.Any(e => e.Id == id);
+        }
+    }
+}
