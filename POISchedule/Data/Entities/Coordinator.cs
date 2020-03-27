9595dd1012c542fadb144d89e35e888829d520aa
Christopher45588 <martinez101jesus@outlook.com>
2020-03-26 21:50:59 -0600

    Se agregó entidad coordinator

---
+++ POISchedule/Data/Entities/Coordinator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace POISchedule.Data.Entities
+{
+    public class Coordinator
+    {
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        public string FullName => $"{LastName} {FirstName}";
+    }
+
+}
+
