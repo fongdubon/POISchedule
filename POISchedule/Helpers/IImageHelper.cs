d6cc671db9f1e50b2da2ee29dc6456ffe311b4de
Eduardo Fong Dubon <eduardofongdubon@hotmail.com>
2020-04-02 11:56:36 -0600

    Creacion del ImageHelper

---
+++ POISchedule/Helpers/IImageHelper.cs
@@ -0,0 +1,10 @@
+﻿namespace POISchedule.Helpers
+{
+    using Microsoft.AspNetCore.Http;
+    using System.Threading.Tasks;
+
+    public interface IImageHelper
+    {
+        Task<string> UploadImageAsync(IFormFile imageFile, string nameFile, string folder);
+    }
+}
