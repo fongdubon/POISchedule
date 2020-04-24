43676bfe00d4497601bb0ac2269bda7af9ddb9da
Eduardo Fong Dubon <eduardofongdubon@hotmail.com>
2020-04-24 14:42:01 -0500

    Creación de clase User Creación de Interaface y Clase UserHelper Creación de interafce y clase CombosHelper

---
+++ POISchedule/Helpers/CombosHelper.cs
@@ -0,0 +1,33 @@
+﻿namespace POISchedule.Helpers
+{
+    using Microsoft.AspNetCore.Mvc.Rendering;
+    using POISchedule.Data;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class CombosHelper : ICombosHelper
+    {
+        private readonly DataContext dataContext;
+
+        public CombosHelper(DataContext dataContext)
+        {
+            this.dataContext = dataContext;
+        }
+
+        public IEnumerable<SelectListItem> GetComboActivities()
+        {
+            var list = dataContext.Activities.Select(
+                c => new SelectListItem
+                {
+                    Text = c.Name,
+                    Value = $"{c.Id}"
+                }).ToList();
+            list.Insert(0, new SelectListItem
+            {
+                Text = "[debe seleccionar una actividad...]",
+                Value = "0"
+            });
+            return list;
+        }
+    }
+}
